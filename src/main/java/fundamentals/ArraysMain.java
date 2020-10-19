52b5bb2689348d4dd7107010d9e805d280a4134c
pabloalonso-20 <alonso.pablo20@gmail.com>
2020-10-19 16:31:44 -0500

    added arraysMain

---
+++ src/main/java/fundamentals/ArraysMain.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class ArraysMain {
+
+	public static void main(String[] args) {
+		
+		int[] ints = {1, 2, 3, 4, 5};
+		
+		System.out.println(ints);
+		
+		for(int i =0; i< ints.length; i++) {
+			System.out.println(ints[i]);
+		}
+
+	}
+
+}
